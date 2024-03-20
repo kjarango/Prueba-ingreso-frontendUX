7e2288f54b49962e380845c612837d9e02dae048
Karen Arango <kjarango64559@umanizales.edu.co>
2024-03-20 16:40:01 -0500

    creacion de los componentes aside, footer, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './core/layout/aside/aside.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { LayoutComponent } from './core/layout/layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    FooterComponent,
+    HeaderComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
