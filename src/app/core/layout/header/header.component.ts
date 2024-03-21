57318afcd4c54c85c16bdd25597c7f8465aefc65
Karen Arango <kjarango64559@umanizales.edu.co>
2024-03-21 17:23:25 -0500

    navbar con el menu lateral funcional en mobil

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header', 
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -9,9 +9,14 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
   isMenuOpen = false;
+  isSubMenuOpen = false;
   
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+    this.isSubMenuOpen = false;
+  }
+  toggleSubMenu(): void {
+    this.isSubMenuOpen = !this.isSubMenuOpen;
   }
 
   ngOnInit(): void {
